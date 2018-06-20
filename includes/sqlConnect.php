03694dd5d8039d7e7d4ba4444879964054fe9845
John Kloor <john@kloor.org>
2018-06-20 11:13:54 -0400

    Store connection in static variable to not repeatedly connect to DB.

--- includes/sqlConnect.php
+++ includes/sqlConnect.php
@@ -2,13 +2,17 @@
 /* Utility function to facilitate database connection */
 function sqlConnect()
 {
-    require __DIR__ .  '/../config/db.php';
-
-    $link = mysql_connect($db_host, $db_user, $db_password);
+    static $link = false;
 
     if (!$link) {
-        die("Couldn't connect to DB: " . mysql_error());
-    }
+        require __DIR__ .  '/../config/db.php';
 
-    mysql_select_db($db_name) or die("Couldn't open DB: " .mysql_error());
+        $link = mysql_connect($db_host, $db_user, $db_password);
+
+        if (!$link) {
+            die("Couldn't connect to DB: " . mysql_error());
+        }
+
+        mysql_select_db($db_name) or die("Couldn't open DB: " .mysql_error());
+    }
 }
